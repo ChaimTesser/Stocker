8c7c120aa0909317767299b316568c15dacd77e3
== <cbtesser@gmail.com>
2020-05-08 00:19:24 -0400

    Initial

---
+++ Stocker/DateStructure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocker
+{
+    public class DateStructure
+    {
+        public DateTimeOffset Date { get; set; }
+        public List<StockAndPrice> StockAndPrices {get;set;}
+        public DateTimeOffset SellDate { get; set; }
+        public DateTimeOffset BoughtDate { get; set; }
+        public bool Bought = false;
+
+    }
+    public class StockAndPrice
+    {
+        public string Stock { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Balance
+    {
+        public DateTime Date { get; set; }
+        public double Inv { get; set; }
+        public double Int { get; set; }
+    }
+
+}
---
+++ Stocker/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stocker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var runData = new RunData();
+            // Start Date, End Date, High, Max, Dtop
+            runData.Run(new DateTime(2019,01, 01), new DateTime(2020, 05, 01), 0.05, 10, -0.3,true);
+         //  runData.LoadData();
+        }
+    }
+
+}
